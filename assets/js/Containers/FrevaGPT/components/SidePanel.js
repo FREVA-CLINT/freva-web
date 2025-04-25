5a742185e46aa11dfff58f372ee27f5f4a440069
BiancaWentzel <courtney_boda@gmx.de>
2025-04-25 10:22:40 +0200

    Added time-dependend sorting and display of threads

--- assets/js/Containers/FrevaGPT/components/SidePanel.js
+++ assets/js/Containers/FrevaGPT/components/SidePanel.js
@@ -3,7 +3,14 @@ import React, { useEffect, useState } from "react";
 import SidePanelCard from "./SidePanelCard";
 
 function SidePanel() {
-  const [threadData, setThreadData] = useState([]);
+  const [threadHistoryData, setThreadHistoryData] = useState({today: [], week: [], month: [], earlier: []})
+
+  function getDate(days) {
+    const date = new Date();
+    date.setDate(date.getDate() - days);
+
+    return date;
+  }
 
   useEffect(() => {
     const threadHistory = [
@@ -16,17 +23,85 @@ function SidePanel() {
           "This is a test regarding your capabilities of using the code_interpreter",
         content: [],
       },
+      {
+        _id: "67dc331e474c83a16dfee836",
+        user_id: "janedoe",
+        thread_id: "noi8RsmgRMR99J3WZQ1SjNxfOEm9bQbj",
+        date: "2025-03-20T15:24:14.604733+00:00",
+        topic: "Please use the code_interpreter tool to run the following code exactly…",
+        content: []
+      },
+      {
+        _id: "67dc335f474c83a16dfee838",
+        user_id: "janedoe",
+        thread_id: "yGNXGSIZuo3uKR0j93Ecs1PwrRi2XaNg",
+        date: "2025-03-20T15:25:19.933795+00:00",
+        topic: "Please assign the value 42 to the variable x in the code_interpreter t…",
+        content: []
+      },
+      {
+        _id: "67dc3373474c83a16dfee839",
+        user_id: "EMPTY USER!! TODO",
+        thread_id: "Su5Gb1zfry4VDlFlBQ4rc7t6mto9vPfM",
+        date: "2025-03-20T15:25:39.030140+00:00",
+        topic: "Please generate a simple xarray dataset in the code_interpreter tool a…",
+        content: []
+      },
+      {
+        _id: "67dc3381474c83a16dfee83a",
+        user_id: "EMPTY USER!! TODO",
+        thread_id: "YHy8XVXKW6iuXTb92HdQUQias1vuCdy4",
+        date: "2025-03-20T15:25:53.942734+00:00",
+        topic: "This is a test request for your basic functionality. Please respond wi…",
+        content: []
+      },
+      {
+        _id: "67dc3385474c83a16dfee83b",
+        user_id: "EMPTY USER!! TODO",
+        thread_id: "jEyscQg0oSHCfeMqDv8Vw0jqqYX8IRmt",
+        date: "2025-03-20T15:25:57.145211+00:00",
+        topic: "Please use the code_interpreter tool to run `print(2938429834 * 234987…",
+        content: []
+      },
+      {
+        _id: "67dc3387474c83a16dfee83c",
+        user_id: "EMPTY USER!! TODO",
+        thread_id: "sCgb80ilU2tEvkPidKSU7rCflDvyhvR7",
+        date: "2025-03-20T15:25:59.209398+00:00",
+        topic: "Please use the code_interpreter tool to run the following code: ",
+        content: []
+      },
+      {
+        _id: "67dc3389474c83a16dfee83d",
+        user_id: "EMPTY USER!! TODO",
+        thread_id: "ufaIFpayMIuxvOPnQwetuf9ybZ7CMwpv",
+        date: "2025-03-20T15:26:11.875477+00:00",
+        topic: "Test: did the conversation continue after stopping the tool call?",
+        content: []
+      }
     ];
 
-    setThreadData(threadHistory);
+    const today = getDate(0);
+    const weekDay = getDate(7);
+    const monthDay = getDate(30);
+
+    const threadDict = {today: [], week: [], month: [], earlier: []};
+
+    threadDict.today = threadHistory.filter(elem => new Date(Date.parse(elem.date)) === today);
+    threadDict.week = threadHistory.filter(elem => new Date(Date.parse(elem.date)) < today && new Date(Date.parse(elem.date)) >= weekDay);
+    threadDict.month = threadHistory.filter(elem => new Date(Date.parse(elem.date)) < weekDay && new Date(Date.parse(elem.date)) >= monthDay);
+    threadDict.earlier = threadHistory.filter(elem => new Date(Date.parse(elem.date)) < monthDay);
+
+    setThreadHistoryData(threadDict);  
+
   }, []);
 
   return (
     <>
-      <SidePanelCard title="Today" data={threadData} />
-      <SidePanelCard title="Last 7 days" data={[]} />
-      <SidePanelCard title="Last 30 days" data={[]} />
-      <SidePanelCard title="Earlier" data={[]} />
+      <SidePanelCard title="Today" data={threadHistoryData.today} />
+      <SidePanelCard title="Last 7 days" data={threadHistoryData.week} />
+      <SidePanelCard title="Last 30 days" data={threadHistoryData.month} />
+      <SidePanelCard title="Earlier" data={threadHistoryData.earlier} />
     </>
   );
 }
--- assets/js/Containers/FrevaGPT/components/SidePanelCard.js
+++ assets/js/Containers/FrevaGPT/components/SidePanelCard.js
@@ -6,6 +6,8 @@ import { Card, OverlayTrigger, Tooltip } from "react-bootstrap";
 
 import { browserHistory } from "react-router";
 
+import { isEmpty } from "lodash";
+
 function SidePanelCard(props) {
   function changeToThread(thread) {
     browserHistory.push({
@@ -15,27 +17,29 @@ function SidePanelCard(props) {
   }
 
   return (
-    <Card className="mb-3 shadow-sm">
-      <Card.Header className="outline-secondary border-0 p-3 rounded-top text-start card-header shadow-sm">
-        {props.title}
-      </Card.Header>
-      <Card.Body className="p-3 py-2">
-        {props.data.map((element) => {
-          return (
-            <div key={element.thread_id} className="mb-2 text-truncate color">
-              <OverlayTrigger
-                key={`${element.topic}-tooltip`}
-                overlay={<Tooltip>{element.topic}</Tooltip>}
-              >
-                <a href="" onClick={() => changeToThread(element.thread_id)}>
-                  {element.topic}
-                </a>
-              </OverlayTrigger>
-            </div>
-          );
-        })}
-      </Card.Body>
-    </Card>
+    <>
+      {!isEmpty(props.data) ? (<Card className="mb-3 shadow-sm">
+        <Card.Header className="outline-secondary border-0 p-3 rounded-top text-start card-header shadow-sm">
+          {props.title}
+        </Card.Header>
+        <Card.Body className="p-3 py-2">
+          {props.data.map((element) => {
+            return (
+              <div key={element.thread_id} className="mb-2 text-truncate color">
+                <OverlayTrigger
+                  key={`${element.topic}-tooltip`}
+                  overlay={<Tooltip>{element.topic}</Tooltip>}
+                >
+                  <a href="" onClick={() => changeToThread(element.thread_id)}>
+                    {element.topic}
+                  </a>
+                </OverlayTrigger>
+              </div>
+            );
+          })}
+        </Card.Body>
+      </Card>) : null}
+    </>
   );
 }
 
